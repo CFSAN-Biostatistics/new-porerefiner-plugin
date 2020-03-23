f5a60b6af65116aa6b7cbe6645b170a86568a230
Justin Payne <justin.payne@fda.hhs.gov>
2020-03-23 15:58:50 -0400

    initial commit

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [{%- if cookiecutter.command_line_interface|lower == 'click' %}'Click>=7.0',{%- endif %} ]
+requirements = ['porerefiner >= 0.8.4',
+                {%- if cookiecutter.command_line_interface|lower == 'click' %}'Click>=7.0',{%- endif %} ]
 
 setup_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest-runner',{%- endif %} ]
 
@@ -27,7 +28,7 @@ test_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest>=3',{%- end
 setup(
     author="{{ cookiecutter.full_name.replace('\"', '\\\"') }}",
     author_email='{{ cookiecutter.email }}',
-    python_requires='>=3.5',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -36,17 +37,13 @@ setup(
 {%- endif %}
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
     description="{{ cookiecutter.project_short_description }}",
     {%- if 'no' not in cookiecutter.command_line_interface|lower %}
     entry_points={
-        'console_scripts': [
-            '{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.cli:main',
-        ],
+        'porerefiner.plugins': '.{{cookiecutter.project_slug}} = {{cookiecutter.project_slug}}',
     },
     {%- endif %}
     install_requires=requirements,
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -1,1 +1,66 @@
 """Main module."""
+
+import dataclasses
+from pathlib import Path
+
+from porerefiner.notifiers import Notifier
+from porerefiner.jobs import FileJob, RunJob
+from porerefiner.jobs.submitter import Submitter
+from porerefiner.model import Run, File
+
+@dataclass
+class {{cookiecutter.project_slug.replace('_',' ').title().replace(' ','')}}Notifier(Notifier):
+    """Configurable run completion notifier. Implement async method 'notify.'"""
+
+    {{cookiecutter.project_slug}}_sample_param: str
+
+    async def notify(self, run: Run, state: NoneType, message: str) -> None:
+        "Handler for notifications. `state` is not currently implemented."
+        pass
+
+@dataclass
+class {{cookiecutter.project_slug.replace('_',' ').title().replace(' ','')}}Submitter(Submitter):
+    """Configurable job runner. Implement the below methods."""
+
+    {{cookiecutter.project_slug}}_sample_param: str
+
+    async def test_noop(self) -> None:
+        "No-op method submitters should implement to make sure the submitter can access an external resource."
+        pass
+
+    def reroot_path(self, path: Path) -> Path:
+        "Submitters should translate paths to be relative to execution environment"
+        pass
+
+    async def begin_job(self, execution_string: str, datadir: Path, remotedir: Path, environment_hints: dict = {}) -> str:
+        "Semantics of scheduling a job. Jobs can provide execution hints. Return an optional job id"
+        pass
+
+    async def poll_job(self, job:str) -> str:
+        "Semantics of polling a job."
+        pass
+
+@dataclass
+class {{cookiecutter.project_slug.replace('_',' ').title().replace(' ','')}}FileJob(FileJob):
+    """Configurable job that will be triggered whenever a file enters a completed state."""
+
+    def setup(self, run: Run, file: File, datadir: Path, remotedir: Path) -> Union[str, Tuple[str, dict]]:
+        "Set up the job. Return a string for the job submitter, and optionally a dictionary of execution hints."
+        pass
+
+    def collect(self, run: Run, file: File, datadir: Path, pid: Union[str, int]) -> None:
+        "Post-job processing. Handle cleanup and make changes to the run or its records."
+        pass
+
+
+@dataclass
+class {{cookiecutter.project_slug.replace('_',' ').title().replace(' ','')}}RunJob(RunJob):
+    """Configurable job that will be triggered whenever a run enters a completed state."""
+
+    def setup(self, run: Run, datadir: Path, remotedir: Path) -> Union[str, Tuple[str, dict]]:
+        "Set up the job. Return a string for the job submitter, and optionally a dictionary of execution hints."
+        pass
+
+    def collect(self, run: Run, datadir: Path, pid: Union[str, int]) -> None:
+        "Post-job processing. Handle cleanup and make changes to the run or its records."
+        pass
